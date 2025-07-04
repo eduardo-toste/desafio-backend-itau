cac91d9a5f65c59d15e39abffa4ee55422d37513
eduardo-toste <duciochetti@gmail.com>
2025-07-04 08:54:53 -0300

    feat: implement validations on create transaction method

--- src/main/java/com/desafio/itau/backend/service/TransacaoService.java
+++ src/main/java/com/desafio/itau/backend/service/TransacaoService.java
@@ -1,10 +1,12 @@
 package com.desafio.itau.backend.service;
 
 import com.desafio.itau.backend.dto.TransacaoRequestDTO;
+import com.desafio.itau.backend.exception.BusinessException;
 import com.desafio.itau.backend.model.Transacao;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -16,6 +18,13 @@ public class TransacaoService {
     List<Transacao> transacoes = new ArrayList<>();
 
     public void criarTransacao(TransacaoRequestDTO request) {
+
+        if (request.valor() == null || request.dataHora() == null) {
+            throw new BusinessException();
+        } else if (request.valor() < 0 || request.dataHora().isAfter(OffsetDateTime.now())) {
+            throw new BusinessException();
+        }
+
         Transacao transacao = new Transacao(UUID.randomUUID(), request.valor(), request.dataHora());
         transacoes.add(transacao);
     }
