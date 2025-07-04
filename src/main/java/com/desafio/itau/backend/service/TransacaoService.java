60751468174f0bbd9fcbb7a2f7661eeec7f43f40
eduardo-toste <duciochetti@gmail.com>
2025-07-03 22:45:11 -0300

    feat: create transaction service with first method

---
+++ src/main/java/com/desafio/itau/backend/service/TransacaoService.java
@@ -0,0 +1,23 @@
+package com.desafio.itau.backend.service;
+
+import com.desafio.itau.backend.dto.TransacaoRequestDTO;
+import com.desafio.itau.backend.model.Transacao;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+@Service
+public class TransacaoService {
+
+    List<Transacao> transacoes = new ArrayList<>();
+
+    public void criarTransacao(TransacaoRequestDTO request) {
+        Transacao transacao = new Transacao(UUID.randomUUID(), request.valor(), request.dataHora());
+        transacoes.add(transacao);
+    }
+
+}
