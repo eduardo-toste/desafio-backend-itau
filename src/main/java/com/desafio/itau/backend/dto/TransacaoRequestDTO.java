73cb8f1980342bccf368de1b6211fe3d720c5bf7
eduardo-toste <duciochetti@gmail.com>
2025-07-04 08:54:36 -0300

    fix: remove jakarta validations from request dto

--- src/main/java/com/desafio/itau/backend/dto/TransacaoRequestDTO.java
+++ src/main/java/com/desafio/itau/backend/dto/TransacaoRequestDTO.java
@@ -1,22 +1,10 @@
 package com.desafio.itau.backend.dto;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.PositiveOrZero;
-import lombok.Value;
-
 import java.time.OffsetDateTime;
 
 public record TransacaoRequestDTO(
 
-        @NotNull
-        @PositiveOrZero
-        double valor,
-
-        @NotNull
-        @PastOrPresent
+        Double valor,
         OffsetDateTime dataHora
 
 ) {
